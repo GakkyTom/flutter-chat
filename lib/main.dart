31331e26692258233db068bc73d12213e57dcc0b
GakkyTom <itagaki.tomoya@eversense.co.jp>
2021-12-31 01:40:12 +0900

    implement with riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'view/login.dart';
+
+final userProvider = StateProvider((ref) {
+  return FirebaseAuth.instance.currentUser;
+});
+
+final infoTextProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+final emailProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+final passwordProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+final messageProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+final postsQueryProvider = StreamProvider.autoDispose((ref) {
+  return FirebaseFirestore.instance
+      .collection('posts')
+      .orderBy('date')
+      .snapshots();
+});
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
